92e658279162d13c7522d4de5c9c6263ab37f436
Udayshankar2001 <kogatamudayshankar@gamil.com>
2023-12-30 13:28:30 +0530

    Added task 3

---
+++ task3.js
@@ -0,0 +1,15 @@
+
+const h= document.getElementById('main-heading');
+h.textContent='Fruit World';
+
+const he= document.getElementById('header');
+he.style.color='orange';
+he.style.borderBottom='3px solid orange';
+he.style.backgroundColor='green'
+
+const bh= document.getElementById('basket-heading');
+bh.style.color='green';
+
+const ta=document.getElementById('thanks');
+ta.innerHTML ='<p>Please visit us again</p>';
+
